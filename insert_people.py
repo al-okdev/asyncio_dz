ce6f1ab3640a3717431574598eccc2ddaf40d8b9
Alexander <alokdev@mail.ru>
2022-03-17 21:54:50 +0300

    v2

--- insert_people.py
+++ insert_people.py
@@ -1,5 +1,6 @@
 import asyncio
 # import random
+import time
 
 import aiohttp
 import asyncpg
@@ -18,24 +19,27 @@ async def get_person(person_id):
     session = aiohttp.ClientSession()
     response = await session.get(f'{url}/people/{person_id}')
     response_json = await response.json()
-    print(response_json['name'])
-    yield (
-        response_json['name'],
-        response_json['gender']
-    )
     await session.close()
+    if 'name' in response_json:
+        print(response_json['name'])
+        return [(response_json['birth_year'], response_json['eye_color'], " ".join(response_json['films']),
+                  response_json['gender'], response_json['hair_color'], response_json['height'], response_json['homeworld'],
+                  response_json['mass'], response_json['name'], response_json['skin_color'], " ".join(response_json['species']),
+                  " ".join(response_json['starships']), " ".join(response_json['vehicles']))]
+    else:
+        return [('-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-')]
 
-# def gen_users_data(quantity: int):
-#
-#     for _ in range(quantity):
-#         yield (
-#             fake.name(),
-#             random.choice(['a', 'b', 'c', 'd'])
-#         )
 
+
+async def get_people(people_ids):
+    tasks = [asyncio.create_task(get_person(person_id)) for person_id in people_ids]
+    for task in tasks:
+        tasks_result = await task
+        yield tasks_result
 
 async def insert_users(pool: asyncpg.Pool, user_list):
-    query = 'INSERT INTO users (name, gender) VALUES ($1, $2)'
+    query = 'INSERT INTO users (birth_year, eye_color, films, gender, hair_color, height, homeworld, mass, name, ' \
+            'skin_color, species, starships, vehicles) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)'
     async with pool.acquire() as conn:
         async with conn.transaction():
             await conn.executemany(query, user_list)
@@ -44,20 +48,14 @@ async def insert_users(pool: asyncpg.Pool, user_list):
 async def main():
     pool = await asyncpg.create_pool(config.PG_DSN, min_size=20, max_size=20)
 
-    # for users_chunk in chunked(gen_users_data(20), 10):
-    #     tasks.append(asyncio.create_task(insert_users(pool, users_chunk)))
-
-    ###
-    for person_ids_chunk in more_itertools.chunked(range(1, 20), 10):
+    for person_ids_chunk in more_itertools.chunked(range(1, 100), 10):
         tasks = []
-        for person_id in person_ids_chunk:
-            tasks.append(asyncio.create_task(insert_users(pool, [('44', '55')] )))
-        # task_results = await asyncio.gather(*list_of_task)
-    ###
-
+        async for person in get_people(person_ids_chunk):
+            tasks.append(asyncio.create_task(insert_users(pool, person)))
 
     await asyncio.gather(*tasks)
     await pool.close()
 
-if __name__ == '__main__':
-    asyncio.run(main())
+start = time.time()
+asyncio.run(main())
+print('Время работы ', time.time() - start)
